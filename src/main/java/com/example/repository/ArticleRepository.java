035f266af3cc06a441b3fa9f59073ff74e8af7cf
YutaSawagami <yuta.sawagami@rakus-partners.co.jp>
2020-07-13 11:57:41 +0900

    create ArticleRepository

---
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -0,0 +1,16 @@
+package com.example.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ArticleRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate templete;
+	
+	public void findAll() {
+		
+	}
+}
